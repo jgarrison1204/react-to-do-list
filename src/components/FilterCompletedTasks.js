5de971de1ded7fbb41d8ae5069a309b4883cb96e
jgarrison1204 <jgarrison1204@gmail.com>
2017-07-13 16:12:26 -0700

    Added onClick event

--- src/components/FilterCompletedTasks.js
+++ src/components/FilterCompletedTasks.js
@@ -3,7 +3,7 @@ import React from 'react';
 function FilterCompletedTasks(props) {
 	return (
 		<div className='centering'>
-			Items Left To Do: <input id='to-do-tasks' type="checkbox"/>
+			Items Left To Do: <input id='to-do-tasks' type="checkbox" onClick={props.handleFilter}/>
 		</div>
 	);
 }
