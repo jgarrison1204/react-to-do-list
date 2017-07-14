deb71e960283a90da292e36ca88adfbb52d4af90
jgarrison1204 <jgarrison1204@gmail.com>
2017-07-14 16:18:08 -0700

    Bootstrap 4 checkbox style applied

--- src/components/FilterCompletedTasks.js
+++ src/components/FilterCompletedTasks.js
@@ -5,14 +5,12 @@ function FilterCompletedTasks(props) {
 		<div 
 			className='centering-check-box animated fadeInDown'
 			style={{'margin': '.5%'}}>
-			<label 
-				htmlFor='to-do-tasks'
-				style={{'margin':'0'}}
-			>Show Items Left To Do:</label>
-			<div
-				style={{'margin-top' : '3px'}}>
-				<input id='to-do-tasks' type="checkbox" onClick={props.handleFilter}/>
-			</div>
+			{/*bootstrap 4 checkbox formatting applied*/}
+			<label className="custom-control custom-checkbox">
+				<input type="checkbox" className="custom-control-input pull-left" onClick={props.handleFilter}/>
+				<span className="custom-control-indicator"></span>
+				<span className="custom-control-description">Show Items Left To Complete</span>
+			</label>
 		</div>
 	);
 }
