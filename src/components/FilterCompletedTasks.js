c1c9f06379e60d5e08c66bfa52f4269c83f68263
jgarrison1204 <jgarrison1204@gmail.com>
2017-07-14 15:59:04 -0700

    Minor styling tweaks to checkbox

--- src/components/FilterCompletedTasks.js
+++ src/components/FilterCompletedTasks.js
@@ -3,13 +3,16 @@ import React from 'react';
 function FilterCompletedTasks(props) {
 	return (
 		<div 
-			className='centering animated fadeInDown'
+			className='centering-check-box animated fadeInDown'
 			style={{'margin': '.5%'}}>
 			<label 
 				htmlFor='to-do-tasks'
 				style={{'margin':'0'}}
 			>Show Items Left To Do:</label>
-			<input id='to-do-tasks' type="checkbox" onClick={props.handleFilter}/>
+			<div
+				style={{'margin-top' : '3px'}}>
+				<input id='to-do-tasks' type="checkbox" onClick={props.handleFilter}/>
+			</div>
 		</div>
 	);
 }
